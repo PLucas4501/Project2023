ada989836adf150844966f9089417ab4f2c4fb69
StergiosTzirakis <sdi1900188@di.uoa.gr>
2023-10-27 18:11:30 +0300

    created graph that saves nodes

---
+++ src/data_structures/graph.h
@@ -0,0 +1,13 @@
+#inlcude <iostream>
+#include <list>
+#inlude "node.h"
+using namespace std;
+
+
+class Graph{
+    
+    public :
+        Graph(int);
+
+        void addedge(int , int);
+}
--- src/data_structures/main.cpp
+++ src/data_structures/main.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int main(int argc, char *argv[])
 {  
     int index=0;
-    Array KNN[argc];  // initialising the graph with K  neighboors
+    Array KNN[argc];  
     while(index < argc){
         while(!argv[index].eof() ){
-            KNN[index] = new  argv[index];
+            KNN[index] = new  argv[index];  //  initialising the graph with values from the dataset
             index++;
-            count << "inserted " << argv[index] << endl;            
+            count << "inserted " << argv[index] << "at index" << index << endl;            
         }
     }
 
